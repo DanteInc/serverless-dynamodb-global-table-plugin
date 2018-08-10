d90aeb89d7c080a6b1ce64b785e459652b0c7ad4
jgilbert01 <jgilbert01@yahoo.com>
2018-08-10 14:16:33 -0400

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # serverless-dynamodb-global-table-plugin
+
 This plugin manages DynamoDB global tables.
+
+As a service is deployed to each successive region the specified tables are added to their global table. Global tables are created on deploying to the first region.
+
+> This plugin is designed to work in conjunction with the [_serverless-dynamodb-autoscaling-plugin_](https://github.com/DanteInc/serverless-dynamodb-autoscaling-plugin) plugin.
+
+## Install plugin:
+
+```
+npm install --save-dev serverless-dynamodb-global-table-plugin
+```
+
+## serverless.yml:
+
+```
+plugins:
+  - serverless-dynamodb-autoscaling-plugin
+  - serverless-dynamodb-global-table-plugin
+
+custom:
+  autoscaling:
+    - table: Table
+      global: true # simply add the global flag
+      read:
+        ...
+      write:
+        ...
+
+resources:
+  Resources:
+    Table:
+      Type: AWS::DynamoDB::Table
+      ...
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+class Plugin {
+  constructor (serverless, options) {
+    this.hooks = {
+      'after:deploy:deploy': deploy.bind(null, serverless, options)
+    };
+  }
+}
+
+module.exports = Plugin;
+
+const deploy = (serverless, options) => {
+  if (!serverless.service.custom.autoscaling) { return Promise.resolve(); }
+
+  const tap = _tap(serverless);
+  const { Resources } = serverless.service.provider.compiledCloudFormationTemplate;
+
+  return Promise.all(
+    serverless.service.custom.autoscaling
+      .filter(config => config.global)
+      .map(config => ({ tableName: Resources[config.table].Properties.TableName }))
+      .map(uow =>
+        serverless.getProvider('aws').request('DynamoDB', 'describeGlobalTable', {
+          GlobalTableName: uow.tableName
+        })
+          .then(data => ({ ...uow, ...data }))
+          .catch((e) => {
+            serverless.cli.log(e.message);
+            return serverless.getProvider('aws').request('DynamoDB', 'createGlobalTable', {
+              GlobalTableName: uow.tableName,
+              ReplicationGroup: [
+                {
+                  RegionName: options.region
+                }
+              ]
+            })
+              .then(data => ({ ...uow, ...data }))
+              .then(tap)
+              .then(() => serverless.cli.log(`Created global table: ${uow.tableName} with region: ${options.region}`));
+          })
+          .then((uow) => {
+            const { ReplicationGroup } = uow.GlobalTableDescription;
+            if (ReplicationGroup.filter(region => region.RegionName === options.region).length === 0) {
+              return serverless.getProvider('aws').request('DynamoDB', 'updateGlobalTable', {
+                GlobalTableName: uow.tableName,
+                ReplicaUpdates: [
+                  {
+                    Create: {
+                      RegionName: options.region
+                    }
+                  }
+                ]
+              })
+                .then(data => ({ ...uow, ...data }))
+                .then(tap)
+                .then(() => serverless.cli.log(`Updated global table: ${uow.tableName} with region: ${options.region}`));
+            } else {
+              return Promise.resolve(uow)
+                .then(tap)
+                .then(() => serverless.cli.log(`Region: ${options.region} already in global table: ${uow.tableName}`));
+            }
+          })
+      )
+  );
+};
+
+const _tap = serverless => (globalTable) => {
+  serverless.cli.log(`globalTable: ${JSON.stringify(globalTable, null, 2)}`);
+  return globalTable;
+};
